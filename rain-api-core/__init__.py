40d0211b3abdc3acb73b783fd71f07f1280689fc
Ben Barton <benbart@users.noreply.github.com>
2019-06-06 14:53:16 -0800

    PR-2000 initial split-up of former tea/common

---
+++ rain-api-core/__init__.py
@@ -0,0 +1,1 @@
+__all__ = ["aws_util", "chalice_util", "egress_util", "session_util", "urs_util", "view_util"]
---
+++ rain-api-core/general_util.py
@@ -0,0 +1,33 @@
+import logging
+import os
+import sys
+
+
+def get_log():
+    loglevel = os.getenv('LOGLEVEL', 'INFO')
+    log_fmt_str = "%(levelname)s: %(message)s (%(filename)s line %(lineno)d/" + \
+                  os.getenv("BUILD_VERSION", "NOBUILD") + "/" + \
+                  os.getenv('MATURITY', 'DEV') + ")"
+
+    logger = logging.getLogger()
+    for h in logger.handlers:
+        logger.removeHandler(h)
+
+    h = logging.StreamHandler(sys.stdout)
+
+    # use whatever format you want here
+    FORMAT = '%(asctime)s %(message)s'
+    h.setFormatter(logging.Formatter(log_fmt_str))
+    logger.addHandler(h)
+    logger.setLevel(getattr(logging, loglevel))
+
+    if os.getenv("QUIETBOTO", 'TRUE').upper() == 'TRUE':
+        # BOTO, be quiet plz
+        logging.getLogger('boto3').setLevel(logging.ERROR)
+        logging.getLogger('botocore').setLevel(logging.ERROR)
+        logging.getLogger('nose').setLevel(logging.ERROR)
+        logging.getLogger('elasticsearch').setLevel(logging.ERROR)
+        logging.getLogger('s3transfer').setLevel(logging.ERROR)
+        logging.getLogger('urllib3').setLevel(logging.ERROR)
+        logging.getLogger('connectionpool').setLevel(logging.ERROR)
+    return logger
