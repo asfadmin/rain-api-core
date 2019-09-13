bf6b67aafa9ffbc006bff1a8aa683e0207bd7603
Ben Barton <benbart@users.noreply.github.com>
2019-09-13 11:01:14 -0800

    PR-2060 Update get_role_creds to allow for in-region checking

--- rain_api_core/aws_util.py
+++ rain_api_core/aws_util.py
@@ -15,6 +15,7 @@ log = logging.getLogger(__name__)
 secret_cache = {}
 region_list_cache = []
 
+
 def get_region():
     url = 'http://169.254.169.254/latest/dynamic/instance-identity/document'
     try:
@@ -28,6 +29,7 @@ def get_region():
 
 
 aws_region = get_region()
+
 
 def retrieve_secret(secret_name):
 
@@ -105,7 +107,6 @@ def read_s3(bucket, key):
     return obj.get()['Body'].read().decode('utf-8')
 
 
-
 def get_yaml(bucket, file_name):
 
     try:
@@ -114,6 +115,7 @@ def get_yaml(bucket, file_name):
     except ClientError as e:
         log.error('Had trouble getting yaml file s3://{}/{}, {}'.format(bucket, file_name, e))
         raise
+
 
 def get_yaml_file(bucket, key):
 
@@ -129,14 +131,25 @@ def get_yaml_file(bucket, key):
         log.error("Could not download yaml @ s3://{0}/{1}: {2}".format(bucket, key, e))
         sys.exit()
 
-def get_role_creds(user_id=None):
 
+def get_role_creds(user_id:str='', in_region:bool=False):
+    """
+    :param user_id: string with URS username
+    :param in_region: boolean If True a download role that works only in region will be returned
+    :return: Returns a set of temporary security credentials (consisting of an access key ID, a secret access key, and a security token)
+    """
     sts = botoclient('sts')
     if not user_id:
         user_id = 'unauthenticated'
-    download_role_arn = os.getenv('EGRESS_APP_DOWNLOAD_ROLE_ARN')
+
+    if in_region:
+        download_role_arn = os.getenv('EGRESS_APP_DOWNLOAD_ROLE_INREGION_ARN')
+    else:
+        download_role_arn = os.getenv('EGRESS_APP_DOWNLOAD_ROLE_ARN')
+
     log.debug('assuming role: {}, role session username: {}'.format(download_role_arn, user_id))
     return sts.assume_role(RoleArn=download_role_arn, RoleSessionName=user_id)
+
 
 def get_role_session(creds=None, user_id=None):
 
@@ -151,7 +164,9 @@ def get_role_session(creds=None, user_id=None):
 
 
 def get_region_cidr_ranges():
-    # Utility function to download AWS regions
+    """
+    :return: Utility function to download AWS regions
+    """
 
     global region_list_cache                                                                 #pylint: disable=global-statement
 
@@ -173,8 +188,13 @@ def get_region_cidr_ranges():
 
 
 def check_in_region_request(ip_addr:str):
+    """
+    :param ip_addr: string with ip address to be checked for in-regionness
+    :return: boolean True if ip_addr is in_region, False otherwise
+    :type: Boolean
+    """
 
-    for cidr in get_region_cidr_ranges(): # TODO: use cached value for this
+    for cidr in get_region_cidr_ranges():
         #log.debug("Checking ip {0} vs cidr {1}".format(user_ip, cidr))
         if IPAddress(ip_addr) in cidr:
             log.info("IP {0} matched in-region CIDR {1}".format(ip_addr, cidr))
