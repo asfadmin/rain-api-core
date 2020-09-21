9ba550a4018af6cf2ad2cb3b2c640095b7a29f39
Ben Barton <benbart@users.noreply.github.com>
2020-09-21 15:48:39 -0800

    #75 checking if item in map_dict is dict

--- rain_api_core/egress_util.py
+++ rain_api_core/egress_util.py
@@ -81,7 +81,7 @@ def get_bucket_dynamic_path(path_list, b_map):
         # Check if we hit a leaf of the YAML tree
         if (mapping and (isinstance(map_dict, str)) or 'bucket' in map_dict):
             customheaders = {}
-            if 'bucket' in map_dict:
+            if isinstance(map_dict, dict) and 'bucket' in map_dict:
                 bucketname = map_dict['bucket']
                 if 'headers' in map_dict:
                     customheaders = map_dict['headers']
