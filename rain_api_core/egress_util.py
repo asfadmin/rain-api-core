11e48812ee510dd655f82adc95f542f77751b736
Darrall Flowers <dpflowersjr@alaska.edu>
2020-12-08 06:54:26 -0900

    added looped bucket name into the check

--- rain_api_core/egress_util.py
+++ rain_api_core/egress_util.py
@@ -157,7 +157,7 @@ def check_private_bucket(bucket, b_map, optional_uri=""):
     # Check public bucket file:
     if 'PRIVATE_BUCKETS' in b_map:
         for priv_bucket in b_map['PRIVATE_BUCKETS']:
-            if bucket == prepend_bucketname(priv_bucket) and bucket_prefix_ismatch(bucket, b_map, optional_uri):
+            if bucket == prepend_bucketname(priv_bucket) and bucket_prefix_ismatch(priv_bucket, b_map, optional_uri):
                 # This bucket is PRIVATE, return group!
                 return b_map['PRIVATE_BUCKETS'][priv_bucket]
 
@@ -172,7 +172,7 @@ def check_public_bucket(bucket, b_map, optional_uri=""):
         for pub_bucket in b_map['PUBLIC_BUCKETS']:
             #log.debug('is {} the same as {}?'.format(bucket, prepend_bucketname(pub_bucket)))
 
-            if bucket == prepend_bucketname(pub_bucket) and bucket_prefix_ismatch(bucket, b_map, optional_uri):
+            if bucket == prepend_bucketname(pub_bucket) and bucket_prefix_ismatch(pub_bucket, b_map, optional_uri):
                 # This bucket is public!
                 log.debug('found a public, we\'ll take it')
                 return True
